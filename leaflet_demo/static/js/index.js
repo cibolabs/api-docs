04ed4bc0ac3f0467e97f946b30171190ea68b4d4
Sam Gillingham <gillingham.sam@gmail.com>
2025-08-25 10:24:55 +1000

    layer control

--- leaflet_demo/static/js/index.js
+++ leaflet_demo/static/js/index.js
@@ -1,6 +1,8 @@
+"use strict";
 
 // globals
 let g_tsdmlayer = null; 
+let g_nbarlayer = null;
 const g_url = 'https://tiles.pasturekey.cibolabs.com';
 
 function init()
@@ -12,8 +14,17 @@ function init()
         const property_id = form.querySelector('#property_id');
         console.log('User entered:', token.value, property_id.value);
         
-        // create map
+        // create map and zoom in on farm
+        // TODO: can we get extents programmatically?
         let map = L.map('map').setView([-31.349912533186938, 150.13404649761717], 14);
+        
+        // background layer
+        const OSMLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
+        OSMLayer.addTo(map);
+
         
         // get dates for this property_id
         fetch(g_url + '/getimagedates/' + property_id.value, {
@@ -33,6 +44,13 @@ function init()
                 }
             });
             g_tsdmlayer.addTo(map);
+            g_nbarlayer = new L.TileLayerHeaders(g_url + '/nbar/' + last_date + '/' + property_id.value + '/{z}/{x}/{y}', {
+                attribution: '&copy; <a href="https://www.cibolabs.com.au/">CiboLabs</a>',
+                customHeaders: {
+                    'Authorization': 'Bearer ' + token.value,
+                    'Accept': 'image/png'                            
+                }
+            });
             
             // dates
             let slider = L.control.slider(function(value) {
@@ -42,6 +60,12 @@ function init()
               'orientation': 'horizontal', 'size': '1000px', 'collapsed': false,
               'getValue': function(value){return data.dates[value]}});
             slider.addTo(map);
+            
+            // layer control
+            let basemap = {"OpenStreetMap": OSMLayer};
+            let cibolayers = {"TSDM": g_tsdmlayer,
+                "NBAR": g_nbarlayer};
+            let layerControl = L.control.layers(basemap, cibolayers).addTo(map);
         });
     });   
     myDialog.showModal();
@@ -49,11 +73,14 @@ function init()
 
 function newLabel(label, map)
 {
-    console.log(label);
+    const form = myDialog.querySelector('form');
+    const property_id = form.querySelector('#property_id');
     if( g_tsdmlayer )
     {
-        const form = myDialog.querySelector('form');
-        const property_id = form.querySelector('#property_id');
         g_tsdmlayer.setUrl(g_url + '/tsdm/' + label + '/' + property_id.value + '/{z}/{x}/{y}');
+    }
+    if( g_nbarlayer )
+    {
+        g_nbarlayer.setUrl(g_url + '/nbar/' + label + '/' + property_id.value + '/{z}/{x}/{y}');
     }
 }
